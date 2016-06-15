fe526cd2ef7f2f0157a5820f0e7c26b9db0da49e
Denis Payase <lostsoul@yandex-team.ru>
2016-06-15 19:56:14 +0300

    Add implementation

---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = {
+    debounce: function(callback, delay) {
+        var timeoutId;
+
+        return function() {
+            var args = arguments;
+            var timeoutCallback = function() {
+                return callback.apply(this, args);
+            };
+
+            if (timeoutId) {
+                clearTimeout(timeoutId);
+            }
+
+            timeoutId = setTimeout(timeoutCallback, delay);
+        };
+    },
+
+    throttle: function(callback, delay) {
+        var isWaiting = false;
+
+        return function() {
+            if(isWaiting) return;
+
+            callback.apply(this, arguments);
+
+            isWaiting = true;
+            setTimeout(function() { isWaiting = false; }, delay);
+        };
+    }
+};
